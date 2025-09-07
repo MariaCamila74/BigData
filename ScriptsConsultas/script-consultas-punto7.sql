9f64de5892a8face0cdaa53fe4ffbfc6343c552f
MiloDS <152004293+MiloDS@users.noreply.github.com>
2025-09-07 16:41:59 -0500

    Actualizacion de el script de las consultas SQL

--- ScriptsConsultas/script-consultas-punto7.sql
+++ ScriptsConsultas/script-consultas-punto7.sql
@@ -49,3 +49,69 @@ WHERE departamento = 'Antioquia'
 GROUP BY municipio
 ORDER BY total_vendidos DESC
 LIMIT 15;
+
+--7.3
+
+--Consulta sin usar la vista
+/*
+SELECT 
+    D.nombre AS departamento,
+    SUM(O.cantidad) AS total_vendidas
+FROM operaciones O, departamentos D, municipios M, Productos p
+WHERE D.id_departamento = O.id_departamento
+AND M.id_municipio = O.id_municipio
+AND P.id_producto = O.id_producto
+WHERE P.nombre = 'MANZALOCA'
+GROUP BY D.nombre
+ORDER BY total_vendidas DESC
+LIMIT 5;
+*/
+
+--Consulta usando la vista
+
+SELECT 
+    departamento,
+    SUM(cantidad) AS total_vendidas
+FROM vista_operaciones
+WHERE producto = 'MANZALOCA'
+GROUP BY departamento
+ORDER BY total_vendidas DESC
+LIMIT 5;
+
+--7.4
+
+--Consulta sin usar la vista
+
+/*
+SELECT 
+    D.nombre AS departamento,
+    M.nombre AS municipio,
+    SUM(O.cantidad * P.precio) AS monto_total
+FROM operaciones O, departamentos D, municipios M, productos P
+WHERE D.id_departamento = O.id_departamento
+AND M.id_municipio = O.id_municipio
+AND P.id_producto = O.id_producto
+WHERE P.nombre = 'MANZALOCA'   
+GROUP BY D.nombre, M.nombre
+ORDER BY monto_total ASC
+LIMIT 5;
+*/
+
+--Consulta usando la vista
+
+SELECT 
+    departamento,
+    municipio,
+    SUM(venta) AS monto_total
+FROM vista_operaciones
+WHERE producto = 'MANZALOCA'
+GROUP BY departamento, municipio
+ORDER BY monto_total ASC
+LIMIT 5;
+
+--7.5
+
+--Consulta sin usar la vista
+
+
+
