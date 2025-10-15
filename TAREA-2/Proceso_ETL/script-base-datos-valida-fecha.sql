5e7bdb5758df7c8fcff8dd1e82dfaadf02e1afe9
MiloDS <152004293+MiloDS@users.noreply.github.com>
2025-10-15 14:46:57 -0500

    Addicion de la carpeta TAREA-4

--- TAREA-2/Proceso_ETL/script-base-datos-valida-fecha.sql
+++ TAREA-2/Proceso_ETL/script-base-datos-valida-fecha.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:TAREA-2/Archivos_Profesor/script-base-datos-valida-fecha.sql
 
 
 -- Consulta SQL
@@ -10,16 +9,3 @@ SELECT * FROM operaciones WHERE fecha !~ '^\d{4}-\d{2}-\d{2}$';
 -- Formato de fecha válido = "AAAA-MM-DD"
 -- Seleccionar los registros con fechas IGUALES al formato
 SELECT * FROM operaciones WHERE fecha ~ '^\d{4}-\d{2}-\d{2}$';
-=======
-
-
--- Consulta SQL
--- Formato de fecha válido = "AAAA-MM-DD"
--- Seleccionar los registros con fechas DIFERENTES al formato
-SELECT * FROM operaciones WHERE fecha !~ '^\d{4}-\d{2}-\d{2}$';
-
--- Consulta SQL
--- Formato de fecha válido = "AAAA-MM-DD"
--- Seleccionar los registros con fechas IGUALES al formato
-SELECT * FROM operaciones WHERE fecha ~ '^\d{4}-\d{2}-\d{2}$';
->>>>>>> 1b7bb061541c7fd4355a4bd7d37a54bff4e26974:script-base-datos-valida-fecha.sql
